56bd1617374d9ddb1227abbe9e39598607b4e23b
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-26 20:04:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,13 +46,4 @@ def turn_count
   end
 
   end
-  def turn
-      puts "Please enter 1-9:"
-      input = gets.strip
-      if valid_move?(input)
-        move(input, current_player)
-      else
-        turn
-      end
-      display_board
-    end
+  
