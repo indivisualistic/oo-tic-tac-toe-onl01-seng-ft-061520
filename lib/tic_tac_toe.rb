33d0e1668c294fd33a0e7d63f7fbb9ade0a96640
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-26 20:22:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,6 +67,18 @@ def turn_count
       end
       return false
   end
+
+def full?
+    @board.all? { |i| i =="X" || i == "O"}
+  end
+
+  def draw?
+    !won? && full? ? true : false
+  end
+
+  def over?
+    won? || draw? || full? ? true : false
+  end
 end
   end
 end
